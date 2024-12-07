26905e582eb36ab55a4cbd44dfd0adf1af9086ea
CristianBMJ <cpmartnez@ug.uchile.cl>
2024-12-07 16:33:01 -0300

    update features folder

--- features/auth.py
+++ features/auth.py
@@ -1,10 +1,15 @@
 from core.database import get_db_connection
+import sqlite3
 
 def register_user(username):
     with get_db_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO users (username) VALUES (?)', (username,))
-        conn.commit()
+        try:
+            cursor.execute('INSERT INTO users (username) VALUES (?)', (username,))
+            conn.commit()
+            return True  # Indicate success
+        except sqlite3.IntegrityError:
+            return False  # Indicate that the username already exists
 
 def get_user_id(username):
     with get_db_connection() as conn:
